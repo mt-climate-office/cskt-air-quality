e04d699fc208a8d413288fe8f4cbd7560be49bc5
bocinsky <bocinsky@gmail.com>
2025-01-08 08:49:59 -0700

    update station names

--- sensor_urls.R
+++ sensor_urls.R
@@ -21,16 +21,16 @@ httr::GET("https://c36nxc3vd0.execute-api.us-west-2.amazonaws.com/prod/sensor-da
   tibble::as_tibble() %>%
   dplyr::transmute(
     `Station ID` = dplyr::case_match(name,
-                                     "DEQ018" ~ 'CSKT — Arlee High School ("DEQ018")',
-                                     "Arlee HS" ~ 'CSKT — Arlee High School (Classroom) ("Arlee HS")',
-                                     "Hot Springs School District" ~ 'CSKT — Hot Springs High School ("Hot Springs School District")',
-                                     "CSKT-Polson Middle School" ~ 'CSKT — Polson Middle School ("CSKT-Polson Middle School")',
-                                     "Ronan, MT" ~ 'CSKT — Ronan High School ("Ronan, MT")',
-                                     "St. Ignatius High School" ~ 'CSKT — St. Ignatius High School ("St. Ignatius High School")',
+                                     "DEQ018" ~ 'CSKT — Arlee High School (Outside, "DEQ018")',
+                                     "Arlee HS" ~ 'CSKT — Arlee High School (Inside, "Arlee HS")',
+                                     "Hot Springs School District" ~ 'CSKT — Hot Springs High School (Outside, "Hot Springs School District")',
+                                     "CSKT-Polson Middle School" ~ 'CSKT — Polson Middle School (Outside, "CSKT-Polson Middle School")',
+                                     "Ronan, MT" ~ 'CSKT — Ronan High School (Outside, "Ronan, MT")',
+                                     "St. Ignatius High School" ~ 'CSKT — St. Ignatius High School (Outside, "St. Ignatius High School")',
                                      .default = name
     )  ,
     `Registration ID` = sensor_index,
     URL = paste0("https://map.purpleair.com/1/b/lt/mAQI/a10/p0/cC0?select=", sensor_index,"#8.6/47.5061/-114.347")
   ) %>%
   dplyr::arrange(`Station ID`) %T>%
-  readr::write_csv("sensor_urls.csv")
+  readr::write_csv("sensor_urls.csv") 
